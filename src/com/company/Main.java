4cda4e06c8caa27819e615df5d2e70a54590d0c0
Ryshoe <angelonicopascua@yahoo.com>
2022-01-30 00:56:33 -0800

    Testing JDBC interfaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import helper.FruitsQuery;
 import helper.JDBC;
+
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         JDBC.openConnection();
+
+        FruitsQuery.select(3);
 
         JDBC.closeConnection();
     }
