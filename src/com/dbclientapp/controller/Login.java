6034147311c638a4996995e618d1a1260e703c96
Ryshoe <angelonicopascua@yahoo.com>
2022-02-05 03:34:38 -0800

    Create GUI elements

--- src/com/dbclientapp/controller/Login.java
+++ src/com/dbclientapp/controller/Login.java
@@ -1,4 +1,22 @@
 package com.dbclientapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Login {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
 }
--- src/com/dbclientapp/controller/MainScreen.java
+++ src/com/dbclientapp/controller/MainScreen.java
@@ -1,4 +1,33 @@
 package com.dbclientapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class MainScreen {
+
+    @FXML
+    private Button appointmentAddButton;
+
+    @FXML
+    private Button appointmentDeleteButton;
+
+    @FXML
+    private Button appointmentEditButton;
+
+    @FXML
+    private TableView<?> appointmentTable;
+
+    @FXML
+    private Button customerAddButton;
+
+    @FXML
+    private Button customerDeleteButton;
+
+    @FXML
+    private Button customerEditButton;
+
+    @FXML
+    private TableView<?> customerTable;
+
 }
