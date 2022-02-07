47a7c9564fd0a0299454a8eeee3b5d0675117ab5
Ryshoe <angelonicopascua@yahoo.com>
2022-02-06 23:08:27 -0800

    Create GUI elements

--- src/com/dbclientapp/controller/CustomerAdd.java
+++ src/com/dbclientapp/controller/CustomerAdd.java
@@ -1,4 +1,53 @@
 package com.dbclientapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class CustomerAdd {
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private ComboBox<?> contactBox;
+
+    @FXML
+    private TextField customerIdField;
+
+    @FXML
+    private TextField descriptionField;
+
+    @FXML
+    private DatePicker endDate;
+
+    @FXML
+    private ComboBox<?> endTime;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField locationField;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private DatePicker startDate;
+
+    @FXML
+    private ComboBox<?> startTime;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private TextField typeField;
+
+    @FXML
+    private TextField userIdField;
+
 }
