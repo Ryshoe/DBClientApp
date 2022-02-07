4dc2bdb82aea66a1c46ab825ab453b0415ebb18b
Ryshoe <angelonicopascua@yahoo.com>
2022-02-06 19:13:53 -0800

    Create GUI elements

--- src/com/dbclientapp/controller/AppointmentAdd.java
+++ src/com/dbclientapp/controller/AppointmentAdd.java
@@ -1,4 +1,37 @@
 package com.dbclientapp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class AppointmentAdd {
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private ComboBox<?> countryBox;
+
+    @FXML
+    private ComboBox<?> divisionBox;
+
+    @FXML
+    private TextField idField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private TextField postalField;
+
 }
