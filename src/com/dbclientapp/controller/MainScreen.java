e45d3e14db97209573e5a9f92c8c66cf3c311f39
Ryshoe <angelonicopascua@yahoo.com>
2022-02-10 02:24:50 -0800

    Update GUI

--- src/com/dbclientapp/controller/MainScreen.java
+++ src/com/dbclientapp/controller/MainScreen.java
@@ -2,8 +2,11 @@ package com.dbclientapp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
 
 public class MainScreen {
 
@@ -17,7 +20,61 @@ public class MainScreen {
     private Button appointmentEditButton;
 
     @FXML
-    private TableView<?> appointmentTable;
+    private TableColumn<?, ?> apptContactCol;
+
+    @FXML
+    private TableColumn<?, ?> apptCustIdCol;
+
+    @FXML
+    private TableColumn<?, ?> apptDescrCol;
+
+    @FXML
+    private TableColumn<?, ?> apptEndCol;
+
+    @FXML
+    private TableColumn<?, ?> apptIdCol;
+
+    @FXML
+    private TableColumn<?, ?> apptLocationCol;
+
+    @FXML
+    private TableColumn<?, ?> apptStartCol;
+
+    @FXML
+    private TableView<?> apptTable;
+
+    @FXML
+    private TableColumn<?, ?> apptTitleCol;
+
+    @FXML
+    private TableColumn<?, ?> apptTypeCol;
+
+    @FXML
+    private TableColumn<?, ?> apptUserIdCol;
+
+    @FXML
+    private TableColumn<?, ?> custAddressCol;
+
+    @FXML
+    private TableColumn<?, ?> custCountryCol;
+
+    @FXML
+    private TableColumn<?, ?> custDivisionCol;
+
+    @FXML
+    private TableColumn<?, ?> custIdCol;
+
+    @FXML
+    private TableColumn<?, ?> custNameCol;
+
+    @FXML
+    private TableColumn<?, ?> custPhoneCol;
+
+    @FXML
+    private TableColumn<?, ?> custPostalCol;
+
+    @FXML
+    private TableView<?> custTable;
 
     @FXML
     private Button customerAddButton;
@@ -29,13 +86,79 @@ public class MainScreen {
     private Button customerEditButton;
 
     @FXML
-    private TableView<?> customerTable;
-
-    @FXML
     private RadioButton monthRadio;
 
     @FXML
     private RadioButton noneRadio;
+
+    @FXML
+    private TableColumn<?, ?> report1MonthColumn;
+
+    @FXML
+    private TableView<?> report1Table;
+
+    @FXML
+    private TableColumn<?, ?> report1TypeColumn1;
+
+    @FXML
+    private TableColumn<?, ?> report1TypeColumn2;
+
+    @FXML
+    private TableColumn<?, ?> report1TypeColumn3;
+
+    @FXML
+    private TableColumn<?, ?> report1TypeColumn4;
+
+    @FXML
+    private TableColumn<?, ?> report2ApptIdCol;
+
+    @FXML
+    private ComboBox<?> report2Box;
+
+    @FXML
+    private TableColumn<?, ?> report2CustIdCol;
+
+    @FXML
+    private TableColumn<?, ?> report2DescrCol;
+
+    @FXML
+    private TableColumn<?, ?> report2EndCol;
+
+    @FXML
+    private Button report2OkButton;
+
+    @FXML
+    private TableColumn<?, ?> report2StartCol;
+
+    @FXML
+    private TableView<?> report2Table;
+
+    @FXML
+    private TableColumn<?, ?> report2TitleCol;
+
+    @FXML
+    private TableColumn<?, ?> report2TypeCol;
+
+    @FXML
+    private TableColumn<?, ?> report3CustIdCol;
+
+    @FXML
+    private TableColumn<?, ?> report3CustNameCol;
+
+    @FXML
+    private TableColumn<?, ?> report3LastUpdtByCol;
+
+    @FXML
+    private TableView<?> report3Table;
+
+    @FXML
+    private ComboBox<?> reportBox;
+
+    @FXML
+    private Button reportRunButton;
+
+    @FXML
+    private ToggleGroup toggleGroup;
 
     @FXML
     private RadioButton weekRadio;
