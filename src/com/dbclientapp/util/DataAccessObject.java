c55b2ce4759ee1c401a7513e20f455bc80967245
Ryshoe <angelonicopascua@yahoo.com>
2022-02-15 02:38:18 -0800

    Remove unused code

--- src/com/dbclientapp/util/DataAccessObject.java
+++ src/com/dbclientapp/util/DataAccessObject.java
@@ -1,42 +1,22 @@
 package com.dbclientapp.util;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 public abstract class DataAccessObject <T extends DataTransferObject> {
 
     protected final Connection connection;
-    protected final static String LAST_VAL = "SELECT last_value FROM ";
 
-//    Handles database connection
-    protected DataAccessObject(Connection connection) {
+    // Handles database connection
+    public DataAccessObject(Connection connection) {
         super();
         this.connection = connection;
     }
 
-//    Defines CRUD methods
+    // Defines CRUD methods
     public abstract T findById(int id);
     public abstract List<T> findAll();
     public abstract T update(T dto);
     public abstract T create(T dto);
     public abstract void delete(int id);
-
-    protected int getLastVal(String sequence) {
-        int key = 0;
-        String sql = LAST_VAL + sequence;
-
-        try(Statement statement = connection.createStatement()) {
-            ResultSet rs = statement.executeQuery(sql);
-            while(rs.next()) {
-                key = rs.getInt(1);
-            }
-            return key;
-        } catch(SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
 }
