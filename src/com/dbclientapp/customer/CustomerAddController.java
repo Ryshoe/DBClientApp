33682317a2a5b96dbd99d1cd90ca5b291629af9f
Ryshoe <angelonicopascua@yahoo.com>
2022-02-22 23:10:36 -0800

    Add methods to populate ComboBoxes

--- src/com/dbclientapp/customer/CustomerAddController.java
+++ src/com/dbclientapp/customer/CustomerAddController.java
@@ -1,8 +1,11 @@
 package com.dbclientapp.customer;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,37 +14,106 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CustomerAddController {
+public class CustomerAddController implements Initializable {
 
     Stage stage;
     Parent scene;
     FXMLLoader loader;
+    private final ObservableList<String> countryList = FXCollections.observableArrayList(
+            "U.S",
+            "UK",
+            "Canada");
+    private final ObservableList<String> divisionListUS = FXCollections.observableArrayList(
+            "Alabama",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "District of Columbia",
+            "Florida",
+            "Georgia",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "Montana",
+            "Nebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming",
+            "Hawaii",
+            "Alaska");
+    private final ObservableList<String> divisionListUK = FXCollections.observableArrayList(
+            "England",
+            "Wales",
+            "Scotland",
+            "Northern Ireland");
+    private final ObservableList<String> divisionListCA = FXCollections.observableArrayList(
+            "Northwest Territories",
+            "Alberta",
+            "British Columbia",
+            "Manitoba",
+            "New Brunswick",
+            "Nova Scotia",
+            "Prince Edward Island",
+            "Ontario",
+            "Québec",
+            "Saskatchewan",
+            "Nunavut",
+            "Yukon",
+            "Newfoundland and Labrador");
 
     @FXML
     private TextField addressField;
-
     @FXML
     private Button cancelButton;
-
     @FXML
-    private ComboBox<?> countryBox;
-
+    private ComboBox<String> countryBox;
     @FXML
     private TextField customerIdField;
-
     @FXML
-    private ComboBox<?> divisionBox;
-
+    private ComboBox<String> divisionBox;
     @FXML
     private TextField nameField;
-
     @FXML
     private Button okButton;
-
     @FXML
     private TextField phoneField;
-
     @FXML
     private TextField postalField;
 
@@ -59,6 +131,21 @@ public class CustomerAddController {
         //TODO Parse inputs from form and add to database
     }
 
-    //TODO Create method that populates country ComboBox
-    //TODO Create method that populates division ComboBox
+    @FXML
+    void countryBoxAction(ActionEvent event) {
+        String selectedCountry = countryBox.getSelectionModel().getSelectedItem();
+
+        // Populate division ComboBox depending on country selection
+        switch (selectedCountry) {
+            case "U.S" -> divisionBox.setItems(divisionListUS);
+            case "UK" -> divisionBox.setItems(divisionListUK);
+            case "Canada" -> divisionBox.setItems(divisionListCA);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Populate country ComboBox
+        countryBox.setItems(countryList);
+    }
 }
