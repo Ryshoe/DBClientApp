02f2c8ee299f16c20403e6556269604cef97b401
Ryshoe <angelonicopascua@yahoo.com>
2022-02-22 19:40:17 -0800

    Change variable from String to StringProperty

--- src/com/dbclientapp/contact/Contact.java
+++ src/com/dbclientapp/contact/Contact.java
@@ -1,12 +1,20 @@
 package com.dbclientapp.contact;
 
 import com.dbclientapp.util.DataTransferObject;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class Contact implements DataTransferObject {
 
     private int id;
-    private String contactName;
+    private final StringProperty contactName = new SimpleStringProperty();
     private String email;
+
+    public Contact(int id, String contactName, String email) {
+        this.id = id;
+        this.contactName.set(contactName);
+        this.email = email;
+    }
 
     @Override
     public int getId() {
@@ -19,11 +27,15 @@ public class Contact implements DataTransferObject {
     }
 
     public String getContactName() {
-        return contactName;
+        return contactName.get();
     }
 
     public void setContactName(String contactName) {
-        this.contactName = contactName;
+        this.contactName.set(contactName);
+    }
+
+    public StringProperty contactNameProperty() {
+        return contactName;
     }
 
     public String getEmail() {
