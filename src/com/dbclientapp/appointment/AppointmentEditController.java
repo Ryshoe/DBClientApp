ef941522197cdb71c6f445493d3abd47b4f97d76
Ryshoe <angelonicopascua@yahoo.com>
2022-02-25 05:13:35 -0800

    Include additional timestamps for appointment start/end times

--- src/com/dbclientapp/appointment/AppointmentEditController.java
+++ src/com/dbclientapp/appointment/AppointmentEditController.java
@@ -44,6 +44,14 @@ public class AppointmentEditController implements Initializable {
             "Team-Building",
             "Feedback");
     private static final ObservableList<String> timeList = FXCollections.observableArrayList(
+            "00:00:00",
+            "01:00:00",
+            "02:00:00",
+            "03:00:00",
+            "04:00:00",
+            "05:00:00",
+            "06:00:00",
+            "07:00:00",
             "08:00:00",
             "09:00:00",
             "10:00:00",
@@ -53,7 +61,13 @@ public class AppointmentEditController implements Initializable {
             "14:00:00",
             "15:00:00",
             "16:00:00",
-            "17:00:00");
+            "17:00:00",
+            "18:00:00",
+            "19:00:00",
+            "20:00:00",
+            "21:00:00",
+            "22:00:00",
+            "23:00:00");
     private final SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
 
     @FXML
