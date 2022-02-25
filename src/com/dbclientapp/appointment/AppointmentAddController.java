e1f5ef25312d811570a9f0186ca276cc86e4b01c
Ryshoe <angelonicopascua@yahoo.com>
2022-02-24 23:44:39 -0800

    Create method to add new appointment

--- src/com/dbclientapp/appointment/AppointmentAddController.java
+++ src/com/dbclientapp/appointment/AppointmentAddController.java
@@ -1,6 +1,12 @@
 package com.dbclientapp.appointment;
 
+import com.dbclientapp.contact.Contact;
+import com.dbclientapp.contact.ContactDAO;
+import com.dbclientapp.customer.Customer;
+import com.dbclientapp.login.LoginController;
 import com.dbclientapp.mainscreen.MainScreenController;
+import com.dbclientapp.user.User;
+import com.dbclientapp.util.DatabaseConnectionManager;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,12 +21,15 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AppointmentAddController implements Initializable {
 
+    private final Customer customer = MainScreenController.getSelectedCustomer();
+    private final User user = LoginController.getLoggedInUser();
     private final ObservableList<String> contactList = FXCollections.observableArrayList(
             "Anika Costa",
             "Daniel Garcia",
@@ -78,27 +87,32 @@ public class AppointmentAddController implements Initializable {
 
     @FXML
     void okButtonAction(ActionEvent event) throws IOException {
-        //TODO Parse inputs from form and add to database
+        parseData();
         returnToMainScreen(event, true);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         populateComboBox();
+        populateTextField();
     }
 
     private void returnToMainScreen(ActionEvent event, boolean check) throws IOException {
         Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../mainscreen/MainScreen.fxml"));
         Parent scene = loader.load();
-
         if(check) {
             MainScreenController mainScreenController = loader.getController();
             mainScreenController.selectTabPane(1);
         }
-
         stage.setScene(new Scene(scene));
         stage.show();
+    }
+
+    private void populateTextField() {
+        // Populate Customer and User ID fields from MainScreen selection
+        customerIdField.setText(String.valueOf(customer.getId()));
+        userIdField.setText(String.valueOf(user.getId()));
     }
 
     private void populateComboBox() {
@@ -109,6 +123,31 @@ public class AppointmentAddController implements Initializable {
         endTime.setItems(timeList);
     }
 
-    //TODO Pre-populate form with selected customer/user ID from MainScreen
-    //TODO Error check bounds for start/end date and time
+    private void parseData() {
+        //TODO Error checking / input validation
+        // Check bounds for start/end date and time
+
+        // Parse input from TextFields
+        Appointment appointmentInput = new Appointment();
+        appointmentInput.setTitle(titleField.getText());
+        appointmentInput.setDescription(descriptionField.getText());
+        appointmentInput.setLocation(locationField.getText());
+        appointmentInput.setCustomer(customer);
+        appointmentInput.setUser(user);
+
+        // Parse contact ComboBox selection
+        ContactDAO contactDAO = new ContactDAO(DatabaseConnectionManager.openConnection());
+        Contact contactInput = contactDAO.findByName(contactBox.getValue());
+        DatabaseConnectionManager.closeConnection();
+        appointmentInput.setContact(contactInput);
+        appointmentInput.setType(typeBox.getValue());
+
+        //TODO Parse date from DatePicker and time from ComboBox
+        // also convert to UTC
+
+        // Create new record in database
+        AppointmentDAO appointmentDAO = new AppointmentDAO(DatabaseConnectionManager.openConnection());
+        appointmentDAO.create(appointmentInput);
+        DatabaseConnectionManager.closeConnection();
+    }
 }
